22350c315dc048d233f621fd3090dfab3b5c5052
Chariselyasa123 <ahmad.charis@raharja.info>
2022-03-02 18:10:23 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import {AuthProvider} from './context/AuthContext';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './components/auth/Login';
+import Home from './components/Home';
+
 function App() {
     return (
-        <p>Samlekom</p>
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                </Routes>
+            </AuthProvider>
+        </Router>
     )
 }
 
