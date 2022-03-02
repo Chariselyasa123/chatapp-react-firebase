2666568f125e27c4a9fa909eb025bbafb1c93cdd
Chariselyasa123 <ahmad.charis@raharja.info>
2022-03-02 17:53:53 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Signup from './components/auth/Signup';
-import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {AuthProvider} from './context/AuthContext';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/PrivateRoute';
 import LupaPassword from './components/auth/LupaPassword';
@@ -14,39 +14,39 @@ import UserChat from './components/chat/UserChat';
 import Home from './components/Home';
 
 function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path='/chats' element={<PrivateRoute />}>
-            <Route path='/chats' element={<Chat />} />
-          </Route>
-          <Route path='/chats/cari-teman' element={<PrivateRoute />}>
-            <Route path='/chats/cari-teman' element={<SearchFriendChat />} />
-          </Route>
-          <Route path='/chat/:id' element={<PrivateRoute />}>
-            <Route path='/chat/:id' element={<UserChat />} />
-          </Route>
-          <Route path='/settings' element={<PrivateRoute />}>
-            <Route path='/settings' element={<Settings />} />
-          </Route>
-          <Route path='/settings/profile' element={<PrivateRoute />}>
-            <Route path='/settings/profile' element={<Profile />} />
-          </Route>
-          <Route path='/teman' element={<PrivateRoute />}>
-            <Route path='/teman' element={<Friend />} />
-          </Route>
-          <Route path='/teman/tambah-teman' element={<PrivateRoute />}>
-            <Route path='/teman/tambah-teman' element={<AddFriend />} />
-          </Route>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path="/daftar" element={<Signup />} />
-          <Route path="/lupa-password" element={<LupaPassword />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  )
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/lupa-password" element={<LupaPassword/>}/>
+                    <Route path='/chats' element={<PrivateRoute/>}>
+                        <Route path='/chats' element={<Chat/>}/>
+                    </Route>
+                    <Route path='/chats/cari-teman' element={<PrivateRoute/>}>
+                        <Route path='/chats/cari-teman' element={<SearchFriendChat/>}/>
+                    </Route>
+                    <Route path='/chat/:id' element={<PrivateRoute/>}>
+                        <Route path='/chat/:id' element={<UserChat/>}/>
+                    </Route>
+                    <Route path='/settings' element={<PrivateRoute/>}>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Route>
+                    <Route path='/settings/profile' element={<PrivateRoute/>}>
+                        <Route path='/settings/profile' element={<Profile/>}/>
+                    </Route>
+                    <Route path='/teman' element={<PrivateRoute/>}>
+                        <Route path='/teman' element={<Friend/>}/>
+                    </Route>
+                    <Route path='/teman/tambah-teman' element={<PrivateRoute/>}>
+                        <Route path='/teman/tambah-teman' element={<AddFriend/>}/>
+                    </Route>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path="/daftar" element={<Signup/>}/>
+                </Routes>
+            </AuthProvider>
+        </Router>
+    )
 }
 
 export default App
