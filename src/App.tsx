40a004eb0b95b6c95b6648987142b200d73810a8
Chariselyasa123 <ahmad.charis@raharja.info>
2022-03-03 15:10:00 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import {AuthProvider} from './context/AuthContext';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './components/auth/Login';
 import Home from './components/Home';
 
 function App() {
     return (
         <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                </Routes>
-            </AuthProvider>
+            {/*<AuthProvider>*/}
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
+            {/*</AuthProvider>*/}
         </Router>
     )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,5 @@
-import {Link} from 'react-router-dom';
-
 export default function Home() {
     return (
-        <Link to="login" className='btn btn-primary'>Login</Link>
+        <button className='btn btn-primary'>Login</button>
     )
 }
